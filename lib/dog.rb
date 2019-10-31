a72441bace02b82e828cee911ec14248adf6bae6
Donovan Jones <donovanljones94@gmail.com>
2019-10-31 15:59:36 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,32 @@
+class Dog
+  attr_accessor :id, :name, :breed
+  
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE dogs(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT)
+      SQL
+      
+      DB[:conn].execute(sql)
+    end
+    
+    def self.drop_table
+      sql = <<-SQL
+      DROP TABLE dogs
+      SQL
+      
+      DB[:conn].execute(sql)
+    end
+    
+    
+      
+  
+  
+end
