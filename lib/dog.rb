e6ef21cb6f64bba39916a9cc655163f4bcd8d49a
Donovan Jones <donovanljones94@gmail.com>
2019-10-31 16:58:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,6 +40,21 @@ class Dog
       
       DB[:conn].execute(sql, name).map { |row| new_from_db(row)}.first
     end
+    
+    def save
+      if self.id
+        self.update
+      else 
+        sql = <<-SQL
+        INSERT INTO dogs (name, grade)
+        VALUES (?,?)
+        SQL
+        
+        DB[:conn].execute(sql, self.name, self.breed)
+        
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      end
+    end
         
       
   
